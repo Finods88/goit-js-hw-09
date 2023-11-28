fd993d077bdf5f128633d3a7fa08dc89ccea1e7f
Alexander <finods@gmail.com>
2023-11-28 19:08:26 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+let timerId = null;
 
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  buttonStop.disabled = true;
+  buttonStart.disabled = false;
+});
